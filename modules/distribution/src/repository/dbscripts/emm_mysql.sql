93ca4f1230b1a813b3725f85871c4abac753a7aa
Chan <dulithaz@gmail.com>
2013-12-02 17:01:27 +0530

    New iOS changes and db script names

--- modules/distribution/src/repository/dbscripts/emm_mysql.sql
+++ modules/distribution/src/repository/dbscripts/emm_mysql.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `mdmdb` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `mdmdb`;
+CREATE DATABASE  IF NOT EXISTS `EMM_DB` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `EMM_DB`;
 -- MySQL dump 10.13  Distrib 5.5.29, for debian-linux-gnu (x86_64)
 --
--- Host: localhost    Database: mdmdb
+-- Host: localhost    Database: EMM_DB
 -- ------------------------------------------------------
 -- Server version	5.5.29-0ubuntu0.12.10.1
 
@@ -22,8 +22,6 @@ USE `mdmdb`;
 --
 
 DROP TABLE IF EXISTS `devices`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `devices` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `tenant_id` int(11) DEFAULT NULL,
@@ -39,12 +37,31 @@ CREATE TABLE `devices` (
   `vendor` varchar(11) DEFAULT NULL,
   `udid` longtext,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
+) ENGINE=InnoDB AUTO_INCREMENT=1022 DEFAULT CHARSET=latin1$$
 --
 -- Dumping data for table `devices`
 --
+DROP TABLE IF EXISTS `device_pending`;
+CREATE TABLE `device_pending` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tenant_id` int(11) DEFAULT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  `platform_id` int(11) DEFAULT NULL,
+  `properties` text,
+  `created_date` datetime DEFAULT NULL,
+  `status` varchar(10) DEFAULT NULL,
+  `byod` smallint(6) DEFAULT '1',
+  `vendor` varchar(11) DEFAULT NULL,
+  `udid` varchar(4096) DEFAULT NULL,
+  `token` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `UDID` (`udid`(767)),
+  KEY `TOKEN` (`token`)
+) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=latin1$$
+
+
+
+
 
 LOCK TABLES `devices` WRITE;
 /*!40000 ALTER TABLE `devices` DISABLE KEYS */;
@@ -91,6 +108,7 @@ CREATE TABLE `features` (
   `deleted` smallint(6) DEFAULT NULL,
   `group_id` int(11) DEFAULT NULL,
   `type_id` int(11) DEFAULT NULL,
+  `monitor` tinyint(4) DEFAULT 0,
   PRIMARY KEY (`id`),
   KEY `fkgroupid` (`group_id`),
   CONSTRAINT `fkgroupid` FOREIGN KEY (`group_id`) REFERENCES `featuregroup` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
@@ -103,7 +121,7 @@ CREATE TABLE `features` (
 
 LOCK TABLES `features` WRITE;
 /*!40000 ALTER TABLE `features` DISABLE KEYS */;
-INSERT INTO `features` VALUES (1,'LOCK','503A','Device Lock',0,1,1),(2,'WIPE','504A','Wipe',0,1,1),(3,'CLEARPASSWORD','505A','Clear',0,1,1),(4,'APPLIST','502A','Get All Applications',0,2,2),(5,'LOCATION','501A','Location',0,1,2),(6,'INFO','500A','Device Information',0,5,2),(7,'NOTIFICATION','506A','Message',0,3,1),(8,'WIFI','507A','Wifi',0,4,1),(9,'CAMERA','508A','Camera',0,1,1),(12,'MUTE','513A','Mute Device',0,1,1),(13,'INSTALLAPP','509A','Install Application',0,2,3),(14,'UNINSTALLAPP','510A','Uninstall Application',0,2,3),(15,'ENCRYPT','511A','Encrypt Storage',0,1,1),(16,'APN','512A','APN',0,4,1),(17,'TRACKCALLS','514A','Track Call Log',0,5,2),(18,'TRACKSMS','515A','Track SMS',0,5,2),(19,'DATAUSAGE','516A','Track Data Usage',0,5,2),(20,'STATUS','517A','Get Device Status',0,5,2),(21,'WEBCLIP','518A','Create Webclips',0,4,3),(22,'PASSWORDPOLICY','519A','Password Policy',0,4,1),(23,'EMAIL','520A','Email Configuration',0,4,1),(24,'GOOGLECALENDAR','521A','Sync Google Calendar',0,4,1),(26,'VPN','513A','VPN',0,4,1),(27,'LDAP','524A','LDAP',0,4,1),(28,'CALENDERSUBSCRIPTION','525A','Calender ',0,4,1),(29,'CHANGEPASSWORD','526A','Set Password',0,4,1),(30,'ENTERPRISEWIPE','527A','Enterprise Wipe',0,1,1),(31,'POLICY','500P','Policy Enforcement',0,1,1),(32,'MONITORING','501P','Policy Monitoring ',0,5,1),(33,'BLACKLISTAPPS','528B','Blacklisting Apps',0,1,1);
+INSERT INTO `features` VALUES (1,'LOCK','503A','Device Lock',0,1,1,0),(2,'WIPE','504A','Wipe',0,1,1,0),(3,'CLEARPASSWORD','505A','Clear',0,1,1,0),(4,'APPLIST','502A','Get All Applications',0,2,2,1),(5,'LOCATION','501A','Location',0,1,2,0),(6,'INFO','500A','Device Information',0,5,2,1),(7,'NOTIFICATION','506A','Message',0,3,1,0),(8,'WIFI','507A','Wifi',0,4,1,0),(9,'CAMERA','508A','Camera',0,1,1,0),(12,'MUTE','513A','Mute Device',0,1,1,0),(13,'INSTALLAPP','509A','Install Application',0,2,3,0),(14,'UNINSTALLAPP','510A','Uninstall Application',0,2,3,0),(15,'ENCRYPT','511A','Encrypt Storage',0,1,1,0),(16,'APN','512A','APN',0,4,1,0),(21,'WEBCLIP','518A','Create Webclips',0,4,3,0),(22,'PASSWORDPOLICY','519A','Password Policy',0,4,1,0),(23,'EMAIL','520A','Email Configuration',0,4,1,0),(24,'GOOGLECALENDAR','521A','Calender Subscription',0,4,1,0),(26,'VPN','523A','VPN',0,4,1,0),(27,'LDAP','524A','LDAP',0,4,1,0),(29,'CHANGEPASSWORD','526A','Set Password',0,4,1,0),(30,'ENTERPRISEWIPE','527A','Enterprise Wipe',0,1,1,0),(31,'POLICY','500P','Policy Enforcement',0,4,1,0),(32,'MONITORING','501P','Policy Monitoring ',0,5,1,1),(33,'BLACKLISTAPPS','528B','Blacklist Apps',0,2,1,0);
 /*!40000 ALTER TABLE `features` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -177,7 +195,7 @@ CREATE TABLE `notifications` (
   `feature_code` varchar(45) DEFAULT NULL,
   `feature_description` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2972 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -229,7 +247,7 @@ CREATE TABLE `platformfeatures` (
   `template` mediumtext,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=68 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=72 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -238,7 +256,7 @@ CREATE TABLE `platformfeatures` (
 
 LOCK TABLES `platformfeatures` WRITE;
 /*!40000 ALTER TABLE `platformfeatures` DISABLE KEYS */;
-INSERT INTO `platformfeatures` VALUES (1,1,1,'2.2','',''),(2,1,2,'2.2','','wipe'),(3,1,3,'2.2','',''),(4,1,4,'2.2','',''),(5,1,5,'2.2','',''),(6,1,6,'2.2','',''),(7,1,7,'2.2','','notifications'),(8,1,8,'2.2','','wifi'),(9,1,9,'4.0','','camera'),(12,1,12,'2.2','',''),(13,2,1,'4.0','5.0',NULL),(15,2,3,'4.0','5.0',''),(16,2,4,'4.0','5.0',NULL),(17,2,6,'4.0','5.0',NULL),(18,2,8,'4.0','5.0','wifi'),(19,2,9,'4.0','5.0','camera'),(21,3,1,'4.0','5.0',NULL),(23,3,3,'4.0','5.0',''),(24,3,4,'4.0','5.0',NULL),(25,3,6,'4.0','5.0',NULL),(26,3,8,'4.0','5.0','wifi'),(28,3,9,'4.0','5.0','camera'),(30,4,1,'4.0','5.0',NULL),(32,4,3,'4.0','5.0',''),(33,4,4,'4.0','5.0',NULL),(34,4,6,'4.0','5.0',NULL),(35,4,8,'4.0','5.0','wifi'),(36,4,9,'4.0','5.0','camera'),(37,1,15,'3.0',NULL,'encrypt'),(38,1,17,'2.2',NULL,NULL),(39,1,18,'2.2',NULL,NULL),(43,1,19,'2.2',NULL,NULL),(44,1,20,'2.2',NULL,NULL),(45,1,21,'2.2',NULL,'webclip'),(46,1,22,'2.2',NULL,'password_policy'),(49,2,21,'4.0','5.0','webclip'),(50,2,22,'4.0','5.0','password_policy'),(51,3,23,'4.0','5.0','email'),(52,3,24,'4.0','5.0','google_calendar'),(53,3,21,'4.0','5.0','webclip'),(54,3,22,'4.0','5.0','password_policy'),(55,2,23,'4.0','5.0','email'),(56,2,24,'4.0','5.0','google_calendar'),(57,1,25,'2.2',NULL,NULL),(58,1,29,'2.2',NULL,'change-password'),(59,2,30,'4.0','5.0',NULL),(60,3,30,'4.0','5.0',NULL),(61,2,16,'4.0','5.0','apn'),(62,3,16,'4.0','5.0','apn'),(63,2,26,'4.0','5.0','vpn'),(64,3,26,'4.0','5.0','vpn'),(65,2,27,'4.0','5.0','ldap'),(66,3,27,'4.0','5.0','ldap'),(67,1,23,'2.2',NULL,'email');
+INSERT INTO `platformfeatures` VALUES (1,1,1,'2.2','',''),(2,1,2,'2.2','','wipe'),(3,1,3,'2.2','',''),(4,1,4,'2.2','',''),(5,1,5,'2.2','',''),(6,1,6,'2.2','',''),(7,1,7,'2.2','','notifications'),(8,1,8,'2.2','','wifi'),(9,1,9,'4.0','','camera'),(12,1,12,'2.2','',''),(13,2,1,'4.0','5.0',NULL),(15,2,3,'4.0','5.0',''),(16,2,4,'4.0','5.0',NULL),(17,2,6,'4.0','5.0',NULL),(18,2,8,'4.0','5.0','wifi'),(19,2,9,'4.0','5.0','camera'),(21,3,1,'4.0','5.0',NULL),(23,3,3,'4.0','5.0',''),(24,3,4,'4.0','5.0',NULL),(25,3,6,'4.0','5.0',NULL),(26,3,8,'4.0','5.0','wifi'),(28,3,9,'4.0','5.0','camera'),(30,4,1,'4.0','5.0',NULL),(32,4,3,'4.0','5.0',''),(33,4,4,'4.0','5.0',NULL),(34,4,6,'4.0','5.0',NULL),(35,4,8,'4.0','5.0','wifi'),(36,4,9,'4.0','5.0','camera'),(37,1,15,'3.0',NULL,'encrypt'),(38,1,17,'2.2',NULL,NULL),(39,1,18,'2.2',NULL,NULL),(43,1,19,'2.2',NULL,NULL),(44,1,20,'2.2',NULL,NULL),(45,1,21,'2.2',NULL,'webclip'),(46,1,22,'2.2',NULL,'password_policy'),(49,2,21,'4.0','5.0','webclip'),(50,2,22,'4.0','5.0','password_policy'),(51,3,23,'4.0','5.0','email'),(52,3,24,'4.0','5.0','google_calendar'),(53,3,21,'4.0','5.0','webclip'),(54,3,22,'4.0','5.0','password_policy'),(55,2,23,'4.0','5.0','email'),(56,2,24,'4.0','5.0','google_calendar'),(57,1,25,'2.2',NULL,NULL),(58,1,29,'2.2',NULL,'change-password'),(59,2,30,'4.0','5.0',NULL),(60,3,30,'4.0','5.0',NULL),(61,2,16,'4.0','5.0','apn'),(62,3,16,'4.0','5.0','apn'),(63,2,26,'4.0','5.0','vpn'),(64,3,26,'4.0','5.0','vpn'),(65,2,27,'4.0','5.0','ldap'),(66,3,27,'4.0','5.0','ldap'),(67,1,23,'2.2',NULL,'email'),(68,1,31,'2.2',NULL,NULL),(69,2,31,'2.2',NULL,NULL),(70,3,31,'2.2',NULL,NULL),(71,4,31,'2.2',NULL,NULL);
 /*!40000 ALTER TABLE `platformfeatures` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -284,7 +302,7 @@ CREATE TABLE `policies` (
   `type` tinyint(4) DEFAULT NULL,
   `category` tinyint(4) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -343,6 +361,45 @@ LOCK TABLES `user_policy_mapping` WRITE;
 /*!40000 ALTER TABLE `user_policy_mapping` DISABLE KEYS */;
 /*!40000 ALTER TABLE `user_policy_mapping` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `device_awake`
+--
+
+DROP TABLE IF EXISTS `device_awake`;
+CREATE  TABLE `EMM_DB`.`device_awake` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT ,
+  `device_id` INT(11) NULL DEFAULT NULL ,
+  `sent_date` DATETIME NULL DEFAULT NULL ,
+  `processed_date` DATETIME NULL DEFAULT NULL ,
+  `call_count` INT(11) NULL DEFAULT 1 ,
+  `status` VARCHAR(1) NULL DEFAULT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `device_id` (`device_id` ASC) )
+COMMENT = 'status will be S (Sent), P (Processed), E (Error / Exception) and D (Deleted)';
+
+
+--
+-- Table structure for table `device_pending`
+--
+
+DROP TABLE IF EXISTS `device_pending`;
+CREATE  TABLE `EMM_DB`.`device_pending` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT ,
+  `tenant_id` INT(11) NULL DEFAULT NULL ,
+  `user_id` VARCHAR(255) NULL DEFAULT NULL ,
+  `platform_id` INT(11) NULL DEFAULT NULL ,
+  `properties` TEXT NULL DEFAULT NULL ,
+  `created_date` DATETIME NULL DEFAULT NULL ,
+  `status` VARCHAR(10) NULL DEFAULT NULL ,
+  `byod` SMALLINT NULL DEFAULT 1 ,
+  `vendor` VARCHAR(11) NULL DEFAULT NULL ,
+  `udid` VARCHAR(4096) NULL DEFAULT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `UDID` (`udid` ASC) );
+
+
+
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -353,4 +410,3 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2013-11-08 15:01:28
